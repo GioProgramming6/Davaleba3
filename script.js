6ec909ee151570ee21e81e8547118f16742b57f6
GioProgramming6 <gioravaxar12@gmail.com>
2025-03-21 23:42:56 +0400

    პირველი ატვირთვა

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("fetch-btn").addEventListener("click", () => {
+    document.getElementById("country-info").innerHTML = `
+        <h2>საქართველო</h2>
+        <p><strong>Population:</strong> 3,140,000</p>
+        <p><strong>Language:</strong> ქართული</p>
+        <img src="https://flagcdn.com/w320/ge.png" alt="Flag of Georgia">
+    `;
+});
